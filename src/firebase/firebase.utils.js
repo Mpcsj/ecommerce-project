e1a845290bc7002e0f47192547733099b6acacf2
mpcsj <marcosjunioribm@gmail.com>
2020-04-11 12:56:16 -0300

    Adding logged user on firebase firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
     appId: "1:379179451150:web:bf1f6476cee33b0b098d5f",
     measurementId: "G-TMVW48V0JQ"
   };
+
+export const createUserProfileDocument = async(userAuth,addionalData)=>{
+  if(!userAuth)return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapShot = await userRef.get()
+  // console.log("teste: ",snapShot)
+  if(!snapShot.exists){
+    // insert on db
+    const {displayName,email} = userAuth
+    const createdAt = new Date()
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...addionalData
+      })
+    }catch(err){
+      console.log('Error creating user: ',err.message)
+    }
+  }
+  return userRef
+}
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
