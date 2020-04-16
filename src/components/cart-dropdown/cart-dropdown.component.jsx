6b7f71401007874ed3d7b1b4a28842ecc1ad33c6
mpcsj <marcosjunioribm@gmail.com>
2020-04-16 16:51:09 -0300

    showing selected products on cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './cart-dropdown.styles.scss'
+import {connect} from 'react-redux'
+import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-buttom.components'
-const CartDropDown=()=>(
+const CartDropDown=({cartItems})=>(
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+            {
+                cartItems.map(item=><CartItem key={item.id} item={item}/>)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-
-export default CartDropDown
+const mapStateToProps = ({cart:{cartItems}})=>({
+    cartItems
+})
+export default connect(mapStateToProps)(CartDropDown)
